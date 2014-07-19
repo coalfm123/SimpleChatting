fce796049655520412cbe530a03139c49530b872
coalfm123 <coalfm123@gmail.com>
2014-07-19 17:34:33 +0900

    client Thread
    Add client Thread exit

--- SimpleChatting_V1_0/clinet/clinet/client.cpp
+++ SimpleChatting_V1_0/clinet/clinet/client.cpp
@@ -49,6 +49,7 @@ DWORD WINAPI receive_data(LPVOID arg) {
 
 	while (1) {
 		if (rflag == 1) return 0;
+
 		retval = recv(sock, buf, BUFSIZE, 0);
 		if (retval == SOCKET_ERROR) {
 			err_display("recv()");
@@ -76,7 +77,7 @@ DWORD WINAPI send_data(LPVOID arg) {
 		if (strcmp(buf, ":exit\n") == 0) {
 			retval = send(sock, buf, BUFSIZE, 0);
 			rflag = 1;
-			break;
+			return 0;
 		}
 		retval = send(sock, buf, BUFSIZE, 0);
 		if (retval == SOCKET_ERROR) err_display("send()");
@@ -118,9 +119,18 @@ int main(int argc, char *argv[]) {
 
 
 	while (1) {
+
 		if (rflag == 1)
 			break;
 	}
+
+	DWORD exitCode;
+
+	GetExitCodeThread(rcvThread, &exitCode);
+	if (exitCode == STILL_ACTIVE) printf("실행중인 쓰레드 : rcvTread\n");
+	GetExitCodeThread(sendThread, &exitCode);
+	if (exitCode == STILL_ACTIVE) printf("실행중인 쓰레드 : sencTread\n");
+
 	closesocket(sock);
 	WSACleanup();
 	return 0;
